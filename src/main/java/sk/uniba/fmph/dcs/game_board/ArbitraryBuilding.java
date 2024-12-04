f85a2c2a8e7da8cfaa94977dfdb681903f0e6156
Thohmass <115932241+Thohmass@users.noreply.github.com>
2024-12-05 00:13:21 +0100

    feat: added extra test to class BuildingTileTest

--- src/main/java/sk/uniba/fmph/dcs/game_board/ArbitraryBuilding.java
+++ src/main/java/sk/uniba/fmph/dcs/game_board/ArbitraryBuilding.java
@@ -2,7 +2,6 @@ package sk.uniba.fmph.dcs.game_board;
 
 import sk.uniba.fmph.dcs.stone_age.Effect;
 
-import java.util.Collection;
 import java.util.OptionalInt;
 
 public final class ArbitraryBuilding implements Building {
